0b37812b09ffae69cb000b2940c2e3361c07adbd
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-08-11 14:01:10 -0400

    building out express backend api

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,20 @@
 const functions = require("firebase-functions");
-
 const express = require("express")
+const cors = require("cors");
+const { response } = require("express");
 
+const stripe = require('stripe')('sk_test_51JNIcRCZzbqBIQx8rRByxnb7bWBNwlk4WIKuUGiKXXm02TDeieWGTNrawt46QncV5Szo1Bf7akLtTYhWquhVl1V700QAoNxTFS')
+
+const app = express()
+
+app.use(cors({ origin: true }))
+
+app.use(express.json())
+
+app.get('/', (request, response) => response.status(200).send('hello world'))
+
+app.post('/payments/create', async (request, response) => {
+  const total = request.query.total
+})
+
+exports.api = functions.https.onRequest(app)
