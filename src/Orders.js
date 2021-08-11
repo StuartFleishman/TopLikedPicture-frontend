fb9bf925da8d0f5d0318f8f43e1a0ffad0db0c03
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-08-11 15:58:29 -0400

    building out oders/order, installed moment

--- src/Orders.js
+++ src/Orders.js
@@ -1,20 +1,46 @@
 import React, { useState, useEffect } from 'react'
+import { db } from './firebase'
+import { connect } from "react-redux"
+import Order from './Order.js'
 import './Orders.css'
 
-function Orders() {
+function Orders({user}) {
   
   const [orders, setOrders] = useState([])
 
   useEffect(() => {
-    
-  }, [])
+    if(user) {
+    db.collection('users').doc(user.user?.uid).collection('orders').orderBy('created', 'desc').onSnapshot(snapshot => (
+      setOrders(snapshot.docs.map(doc => ({
+        id: doc.id, 
+        data: doc.data()
+      })))
+    ))
+    } else {
+      setOrders([])
+    }
+  }, [user])
 
 
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+      <div className="orders__order">
+          {orders?.map(order => (
+            <Order order={order} />
+          ))}
+      </div>
     </div>
   )
 }
 
-export default Orders
+const mapStateToProps = state => {
+  
+  return {
+    cart: state.products.cart,
+    user: state.user,
+    loggedIn: state.user.loggedIn
+  }
+}
+
+export default connect(mapStateToProps)(Orders)
