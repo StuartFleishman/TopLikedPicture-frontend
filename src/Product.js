c00f4ac5be3c10d4e846aaab15d213cc728aff07
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-08-19 13:22:24 -0400

    button changes to smiley face if out of stock, cart now says in cart if added to cart

--- src/Product.js
+++ src/Product.js
@@ -10,17 +10,18 @@ function Product({ id, name, price, description, addToBasket, image, cart, quant
   return (
     <div className="product">
       <div className="product__info">
-  {parseInt(quantity) === 0 ? <h6 style={{color: 'red'}}>out of stock</h6> : <h6>{quantity} in stock</h6>} 
+        {parseInt(quantity) === 0 ? 
+        <h6 style={{color: 'red'}}>out of stock</h6> :   
+          <p className="product__price">
+            <small>$</small>
+            <strong>{price}</strong>
+        </p> }
         <h1>{name}</h1>
-        <h4>{description}</h4>
-        <p className="product__price">
-          <small>$</small>
-          <strong>{price}</strong>
-        </p>
+      
          
       </div>
           <img src={image} height={100} width={100} />
-       {!cart.find(b=> b.id === id) && parseInt(quantity) === 0 ? <SentimentSatisfiedAltIcon /> : <button className="product" onClick={() => addToCart(id) }> CART</button> }
+       {parseInt(quantity) === 0 ? <SentimentSatisfiedAltIcon /> : cart.find(b=> b.id === id) ? <button  className="product" > in cart</button>: <button className="product" onClick={() => addToCart(id) }> CART</button> }
     </div>
   );
 }
